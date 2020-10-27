462a4bf04edeb9533ef2568e7219d76493664c6f
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-28 02:06:01 +0700

    update app

--- app/src/main/java/com/example/ageapp/MainActivity.kt
+++ app/src/main/java/com/example/ageapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.ageapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onSubmit(view: View){
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        tvAge.text = "Age : "+ (currentYear - inputYear.text.toString().toInt())
+    }
+
 }
